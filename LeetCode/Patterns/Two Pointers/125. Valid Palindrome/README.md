a32003b54e84176c406620583931de0252890dad
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-08-27 20:28:51 +0530

    Add Easy Python Solution for 125. Valid Palindrome from LeetCode

--- LeetCode/Patterns/Two Pointers/125. Valid Palindrome/README.md
+++ LeetCode/Patterns/Two Pointers/125. Valid Palindrome/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String
 
 ### 🚀 Performance
-- **Runtime:** 11 ms
-- **Memory:** 23.2 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Python/Easy/125. Valid Palindrome/Solution.py
+++ LeetCode/Python/Easy/125. Valid Palindrome/Solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i = []
-        for c in s:
-            if c.isalnum():
-                i.append(c.lower())
-        s = ''.join(i)
         l = 0
         r = len(s)-1
         while(l<r):
-            if s[l] != s[r]:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            if s[l].lower() != s[r].lower():
                 return False
             l+=1
             r-=1
