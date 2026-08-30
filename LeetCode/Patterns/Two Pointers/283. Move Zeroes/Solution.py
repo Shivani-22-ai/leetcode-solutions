c14a359b3ee939cfff561e0efb3821421ed4b7a1
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-08-30 20:32:37 +0530

    Add Easy Python Solution for 283. Move Zeroes from LeetCode

--- LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.py
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         i = 0
-        for j in range(len(nums)):
-            if nums[j] != 0:
+        j = 1
+        while j<len(nums):
+            if nums[j] != 0 and nums[i]==0:
                 nums[i],nums[j] = nums[j],nums[i]
                 i+=1
+            j+=1
+            
+
         
