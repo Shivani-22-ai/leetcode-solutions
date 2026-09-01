935133af1de999284860ce2c65caf83339f35571
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-09-01 13:39:06 +0530

    Add Easy Python Solution for 202. Happy Number from LeetCode

--- LeetCode/Python/Easy/202. Happy Number/Solution.py
+++ LeetCode/Python/Easy/202. Happy Number/Solution.py
@@ -1,19 +1,19 @@
-def sos(n):
-    s=0
+def sod(n):
+    s = 0
     while(n>0):
-        s = s + (n%10)*(n%10)
+        s = s + n%10
         n = n//10
     return s
-
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n not in seen and n!=1:
-            seen.add(n)
-            n = sos(n)
-        if(n==1):
+        s = set()
+        while n!=1 and sod(n) not in s:
+            n = sod(n)
+            s.add(n)
+        if n==1:
             return True
         else:
             return False
+
 
         
