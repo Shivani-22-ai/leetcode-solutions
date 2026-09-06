b64a393c1ee5f4f7167293ce1f46f3fc198db0a3
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-09-06 19:59:39 +0530

    Add Easy Python Solution for 643. Maximum Average Subarray I from LeetCode

--- LeetCode/Patterns/Sliding Window/643. Maximum Average Subarray I/Solution.py
+++ LeetCode/Patterns/Sliding Window/643. Maximum Average Subarray I/Solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         s = 0
+        mxa = float('-inf')
         for i in range(k):
             s+=nums[i]
-        mx = s/k 
-
-        for r in range(k,len(nums)):
+        mxa = max(mxa,s/k)
+        l=0
+        r=k
+        while(r<len(nums)):
+            s-=nums[l]
+            l+=1
             s+=nums[r]
-            s-=nums[r-k]
-            mx = max(mx,s/k)
-        return mx
-
+            mxa = max(mxa,s/k)
+            r+=1
+        return mxa
         
 
 
