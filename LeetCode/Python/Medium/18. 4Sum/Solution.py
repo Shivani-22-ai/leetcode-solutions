4d954bd8ca05b3accf1e3494aa449ecb19a3ef02
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-09-09 14:52:49 +0530

    Add Medium Python Solution for 18. 4Sum from LeetCode

--- LeetCode/Python/Medium/18. 4Sum/Solution.py
+++ LeetCode/Python/Medium/18. 4Sum/Solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         dis = []
-        nums.
+        nums.sort()
         for i in range(len(nums)-3):
             for j in range(i+1,len(nums)-2):
                 for k in range(j+1,len(nums)-1):
