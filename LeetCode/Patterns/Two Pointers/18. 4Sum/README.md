cbcc43fee262feccd759d0f09cd80ed1671d72bb
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-09-09 14:55:38 +0530

    Add Medium Python Solution for 18. 4Sum from LeetCode

--- LeetCode/Patterns/Two Pointers/18. 4Sum/README.md
+++ LeetCode/Patterns/Two Pointers/18. 4Sum/README.md
@@ -1,6 +1,6 @@
 # 📝 18. 4Sum (LeetCode)
 
-🔗 [Problem Link](https://leetcode.com/problems/4sum/?envType=problem-list-v2&envId=two-pointers)
+🔗 [Problem Link](https://leetcode.com/problems/4sum/)
 
 ![Difficulty](https://img.shields.io/badge/Difficulty-Medium-orange) ![Language](https://img.shields.io/badge/Language-Python-blue)
 
@@ -8,8 +8,8 @@
 Array, Two Pointers, Sorting
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 403 ms
+- **Memory:** 18.8 MB
 
 ---
 
--- LeetCode/Patterns/Two Pointers/18. 4Sum/Solution.py
+++ LeetCode/Patterns/Two Pointers/18. 4Sum/Solution.py
@@ -6,9 +6,22 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i+1,len(nums)-2):
-                for k in range(j+1,len(nums)-1):
-                    for l in range(k+1,len(nums)):
-                        if nums[i]+nums[j]+nums[k]+nums[l] == target:
-                            if [nums[i],nums[j],nums[k],nums[l]] not in dis:
-                                dis.append([nums[i],nums[j],nums[k],nums[l]])
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                k = j + 1
+                l = len(nums) - 1
+                while k < l:
+                    total = nums[i] + nums[j] + nums[k] + nums[l]
+                    if total == target:
+                        dis.append([nums[i], nums[j], nums[k], nums[l]])
+                        while k < l and nums[k] == nums[k + 1]:
+                            k += 1
+                        while k < l and nums[l] == nums[l - 1]:
+                            l -= 1
+                        k += 1
+                        l -= 1
+                    elif total < target:
+                        k += 1
+                    else:
+                        l -= 1
         return dis
