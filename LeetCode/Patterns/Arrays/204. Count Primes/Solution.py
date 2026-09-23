ccfbac95d0e5a6d06b011708e1f8b0bd8c36f232
Lokinindi Shivani <lokinindi.shivani@gmail.com>
2026-09-23 14:51:42 +0530

    Add Medium Python Solution for 204. Count Primes from LeetCode

--- LeetCode/Patterns/Arrays/204. Count Primes/Solution.py
+++ LeetCode/Patterns/Arrays/204. Count Primes/Solution.py
@@ -1,13 +1,13 @@
 import math
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n<2:
+        if n<=2:
             return 0
-        primes = [True]*(n+1)
+        primes = [True]*(n)
         primes[0] = False
         primes[1] = False
         for i in range(2,math.floor(math.sqrt(n))+1):
             if primes[i]:
-                for j in range(i*i,n+1,i):
+                for j in range(i*i,n,i):
                     primes[j] = False
         return sum(primes)    
